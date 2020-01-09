461e19d9c215699637e8d2f6d52237d0ae339409
unknown <7476903@alu.murciaeduca.es>
2020-01-09 09:27:03 +0100

    Faker

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if(env('APP_ENV') != 'production'){
+            DB::table('users')->truncate();
+            $users = factory(App\User::class,100)->create();
+        }
+    }
+}
