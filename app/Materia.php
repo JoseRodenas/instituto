db0c3225218a8541894d02c0c0fe2ed5fca9e298
Mathias <3888592@alu.murciaeduca.es>
2020-01-17 10:40:52 +0100

    para pull

--- app/Materia.php
+++ app/Materia.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Materia extends Model
 {
     protected $table = "materia";
+
+    public function MateriaImpartida ()
+    {
+        return $this->hasMany('App\MateriaImpartida', 'materia');
+    }
+
+    public function MateriaMatriculada ()
+    {
+        return $this->hasMany('App\MateriaMatriculada', 'materia');
+    }
 }
--- app/Materiamatriculada.php
+++ app/Materiamatriculada.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Materiamatriculada extends Model
 {
     protected $table = "materiamatriculada";
+
+    public function Usuario()
+    {
+        return $this->belongsTo('App\Usuario', 'alumno');
+    }
+    public function Grupo()
+    {
+        return $this->belongsTo('App\Grupo', 'grupo');
+    }
+    public function Materia()
+    {
+        return $this->belongsTo('App\Materia', 'materia');
+    }
 }
